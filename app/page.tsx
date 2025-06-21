557631c905d479e72dcf85383b480f90230df674
Yiran <yiran@unknowndec2f2709ed4.attlocal.net>
2025-06-21 17:25:52 -0500

    add bg back on welcome

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import LandingHero from '@/components/landing/LandingHero';
 
 export default function HomePage() {
   return (
-    <>
+    <div className="min-h-screen bg-green-50">
       <LandingHeader />
       <LandingHero />
-    </>
+    </div>
   );
 }
